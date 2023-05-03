90f2a67c9b2ead97f44fe07d5bf81d23b2542f5b
Токарев Павел рис-22-1б <130695318+Pave1Tokarev@users.noreply.github.com>
2023-05-03 16:22:48 +0500

    Add files via upload

---
+++ labclasses9/labclasses9/labclasses9.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include"Error.h"
+#include"Vector.h"
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    try {
+        Vector tmp(5);
+        cin >> tmp;
+        cout << "Ввведите количество элементов для удаления: ";
+        int n;
+        cin >> n;
+        tmp - n;
+        cout << "Введите индекс элемента для вывода на экран: ";
+        cin >> n;
+        cout << tmp[n];
+    }
+    catch (Error err) {
+        err.what();
+    }
+}
