09a6e376fe8ba3c91905c81049b053344a6e2941
Токарев Павел рис-22-1б <130695318+Pave1Tokarev@users.noreply.github.com>
2023-05-15 20:05:30 +0500

    Add files via upload

---
+++ Class7/labclasses7/Time.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+class Time {
+public:
+	int minute;
+	int second;
+	Time(void);
+	Time(int, int);
+	Time(const Time&);
+	~Time();
+	friend ostream& operator<<(ostream&out,const Time&);
+	friend istream& operator>>(istream& in, Time&);
+	Time& operator=(const Time&);
+	Time operator-(const Time&);
+	Time operator+(const Time&);
+	bool operator!=(const Time&);
+};
---
+++ Class7/labclasses7/Vector.cpp
@@ -0,0 +1,23 @@
+﻿#include "Vector.h"
+#include <iostream>
+using namespace std;
+template <typename t>
+Vector<t>::Vector(int s, t k);
+template <typename t>
+Vector<t>::Vector(const Vector<t>& a);
+template<typename t>
+Vector<t>::~Vector();
+template<typename t>
+ostream& operator<< (ostream& out, const Vector<t>& a);
+template<typename t>
+istream& operator>> (istream& in, Vector<t>& a);
+template <typename t>
+int Vector<t>::operator()();
+template <typename t>
+t Vector<t>::operator[](int tm);
+template <typename t>
+Vector<t> Vector<t>::operator+(const Vector<t>&a);
+template <typename t>
+Vector<t>& Vector<t>::operator=(const Vector<t>& a);
+template <typename t>
+void Vector<t>::set_element(int index, t& a);
